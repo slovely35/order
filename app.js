e6c08f448a43d2efbe6d79e907c57affa5939778
wontwopunch <jiwontwopunch@gmail.com>
2024-11-25 22:38:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const connectFlash = require('connect-flash');
 const connectDB = require('./config/db');
